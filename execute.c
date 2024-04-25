643ee64aca3a14dc38d87afe3bc7757a5bf38613
Huang1810 <159008826+Huang1810@users.noreply.github.com>
2024-04-26 00:20:04 +0100

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,93 +1,69 @@
 #include "main.h"
+
 /**
-* execute - executes command
-*
-*@command: The command string to execute.
-*
-* Return: the exit status of the executed command,
-* or -1 if an error occurs.
-*/
-int execute(char *command)
-{
-int status = 0;
-pid_t pid = fork();
-if (pid == -1)
-{
-perror("fork");
-free(command);
-exit(EXIT_FAILURE);
+ * execute_command - Executes the given command.
+ * @command: The command to execute.
+ * Return: The exit status of the executed command.
+ */
+int execute_command(char *command) {
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        perror("fork");
+        free(command);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0) {
+        execute_child_process(command);
+    } else {
+        return wait_child_process(pid, command);
+    }
 }
-else if (pid == 0)
-{
-char *arr[64];
-line_div(command, arr);
-if (arr[0] == NULL)
-{
-free(command);
-exit(EXIT_SUCCESS);
+
+/**
+ * execute_child_process - Executes the command in the child process.
+ * @command: The command to execute.
+ * Return: This function does not return; it exits the child process.
+ */
+void execute_child_process(char *command) {
+    char **args;
+    args = tokenize_command(command);
+
+    if (args[0] == NULL) {
+        free(command);
+        exit(EXIT_SUCCESS);
+    }
+
+    if (strcmp(args[0], "env") == 0) {
+        print_environment();
+    }
+
+    if (strchr(args[0], '/') != NULL) {
+        execute_absolute_path(args);
+    } else {
+        execute_relative_path(args);
+    }
+
+    // If none of the conditions above are met, command not found
+    fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+    free(command);
+    exit(127);
 }
-if (strcmp(arr[0], "env") == 0)
-{
-char **env = environ;
-while (*env != NULL)
-{
-printf("%s\n", *env);
-env++;
-}
-free(command);
-exit(EXIT_SUCCESS);
-}
-if (strchr(arr[0], '/') != NULL)
-{
-if (access(arr[0], X_OK) == 0)
-{
-if (execve(arr[0], arr, environ) == -1)
-{
-perror("execve");
-free(command);
-exit(EXIT_FAILURE);
-}
-}
-}
-else
-{
-char *path = getenv("PATH");
-char *token;
-if (path == NULL)
-{
-fprintf(stderr, "./hsh: 1: %s: not found\n", arr[0]);
-free(command);
-exit(127);
-}
-token = strtok(path, ":");
-while (token != NULL)
-{
-char executable_path[256];
-snprintf(executable_path, sizeof(executable_path), "%s/%s", token, arr[0]);
-if (access(executable_path, X_OK) == 0)
-{
-if (execve(executable_path, arr, environ) == -1)
-{
-perror("execve");
-free(command);
-exit(EXIT_FAILURE);
-}
-}
-token = strtok(NULL, ":");
-}
-}
-fprintf(stderr, "./hsh: 1: %s: not found\n", arr[0]);
-free(command);
-exit(127);
-}
-else
-{
-waitpid(pid, &status, 0);
-free(command);
-if (WIFEXITED(status))
-status = WEXITSTATUS(status);
-else
-status = 1;
-}
-return (status);
+
+/**
+ * wait_child_process - Waits for the child process to finish.
+ * @pid: The process ID of the child process.
+ * @command: The command string.
+ * Return: The exit status of the child process.
+ */
+int wait_child_process(pid_t pid, char *command) {
+    int status;
+    waitpid(pid, &status, 0);
+    free(command);
+    if (WIFEXITED(status)) {
+        return WEXITSTATUS(status);
+    } else {
+        return 1;
+    }
 }
