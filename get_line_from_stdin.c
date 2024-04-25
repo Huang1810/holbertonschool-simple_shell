077a10f2a10bd79946c271ae85cab85599a37a2d
Huang1810 <ahhamed1810@gmail.com>
2024-04-25 13:55:56 +0100

    commit

---
+++ get_line_from_stdin.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _getline - function to implement getline
+* Return: NULL
+*/
+
+char *_getline(void)
+{
+char *line = NULL;
+size_t size = 0;
+int checker;
+checker = getline(&line, &size, stdin);
+if (checker == -1)
+free(line);
+else
+return (line);
+return (NULL);
+}
